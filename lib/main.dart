543d4fae0b22e52e2558399cdc79c3636076810c
Nischal Kathayat <129159364+niscxal@users.noreply.github.com>
2024-07-08 12:19:48 +0545

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:test_app5/Pages/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      home: Homepage(),
+    );
+  }
+}
